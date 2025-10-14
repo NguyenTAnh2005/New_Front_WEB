efe8b7e3b2ef9afe77fac6fe7a2c290232308f8a
NTANH-05 <anhnguyentaun@gmail.com>
2025-10-14 17:03:07 +0700

    Adding padding

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -23,7 +23,7 @@ export function HomePage() {
     return (
         <>
             <Home_Banner services={services} />
-            <div className="flex flex-col items-center bg-white pt-5 pb-10">
+            <div className="flex flex-col items-center bg-white pt-5 pb-10 px-5">
                 <p className="text-black text-[50px] font-semibold capitalize mt-10">Popular Phones</p>
                 <p className="text-[24px] text-gray-600">Discover our best-selling phones</p>
                 <div className="mt-10 px-5 grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-x-7 gap-y-7">
@@ -31,7 +31,7 @@ export function HomePage() {
                 </div>
                 <button className="mt-10 capitalize text-mainCL font-semibold text-[24px] rounded-xl px-3 py-1 hover:scale-90 hover:bg-mainCL hover:-translate-y-1 hover:text-white transition-all duration-300 ease-linear"> View All Phones</button>
             </div>
-            <div className="flex flex-col items-center bg-black pt-5 pb-10 ">
+            <div className="flex flex-col items-center bg-black pt-5 pb-10 px-5">
                 <p className="text-white text-[50px] font-semibold capitalize mt-10">Special Deals</p>
                 <p className="text-[24px] text-gray-600">Limited time offers - Don't miss out!</p>
                 <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-7 gap-y-7">
@@ -39,7 +39,7 @@ export function HomePage() {
                 </div>
                 <button className="mt-10 capitalize text-mainCL font-semibold text-[24px] rounded-xl px-3 py-1 hover:scale-90 hover:bg-mainCL hover:-translate-y-1 hover:text-white transition-all duration-300 ease-linear"> View All Deals</button>
             </div>
-            <div className="flex flex-col items-center bg-white pt-5 pb-10">
+            <div className="flex flex-col items-center bg-white pt-5 pb-10 px-5">
                 <p className="text-black text-[50px] font-semibold capitalize mt-10">Latest Technology Article </p>
                 <p className="text-[24px] text-gray-600">Stay updated with tech news and reviews</p>
                 <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-7 gap-y-7">
